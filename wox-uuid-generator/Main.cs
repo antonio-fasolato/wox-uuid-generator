82e7ca3a82c82957a1a86e303d33b677aeadaf72
ut0729 <fasolato@omicronconsulting.it>
2018-08-07 16:41:59 +0200

    First working example

---
+++ wox-uuid-generator/Main.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Wox.Plugin;
+using System.Windows.Forms;
+
+namespace wox_uuid_generator
+{
+    public class Main : IPlugin
+    {
+        public void Init(PluginInitContext context) {
+            //Init code here
+        }
+
+        public List<Result> Query(Query query) {
+            var results = new List<Result>();
+            var id = Guid.NewGuid().ToString();
+            results.Add(new Result() {
+                Title = id,
+                SubTitle = "Copy to clipboard",
+                IcoPath = "Images\\plugin.png",  //relative path to your plugin directory
+                Action = e => {
+                    // after user selects the item
+                    Clipboard.SetText(id);
+
+                    // return false to tell Wox not to hide thequery window, otherwise Wox will hide it automatically
+                    return true;
+                }
+            });
+            return results;
+        }
+    }
+}
