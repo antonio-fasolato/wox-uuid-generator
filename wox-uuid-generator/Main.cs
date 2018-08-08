75b92a3e3d71cbaa5a32bbc16ea6115fec5eee28
ut0729 <fasolato@omicronconsulting.it>
2018-08-08 12:44:08 +0200

    List of values (optional parameter)

--- wox-uuid-generator/Main.cs
+++ wox-uuid-generator/Main.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Wox.Plugin;
 using System.Windows.Forms;
+using System.Linq;
 
 namespace wox_uuid_generator
 {
@@ -13,44 +14,59 @@ namespace wox_uuid_generator
 
         public List<Result> Query(Query query) {
             var results = new List<Result>();
-            var id = Guid.NewGuid().ToString();
+            var ids = new List<string>();
+
+            int count = 1;
+
+            if(query.Terms.Count() > 1) {
+                int x = 0;
+                if(int.TryParse(query.Terms[1], out x)) {
+                    count = x;
+                }
+            }
+
+            for (int i = 0; i < count; ++i) {
+                ids.Add(Guid.NewGuid().ToString());
+            }
 
             //Basic
             results.Add(new Result() {
-                Title = id,
+                Title = String.Join(", ", ids),
                 SubTitle = "Copy to clipboard",
                 IcoPath = "Images\\icon.png",  //relative path to your plugin directory
                 Action = e => {
                     // after user selects the item
-                    Clipboard.SetText(id);
+                    Clipboard.SetText(String.Join("\n", ids) + "\n");
 
                     // return false to tell Wox not to hide thequery window, otherwise Wox will hide it automatically
                     return true;
                 }
             });
 
+            var uppercase = ids.Select(t => t.ToUpper());
             //Uppercase
             results.Add(new Result() {
-                Title = id.ToUpper(),
+                Title = String.Join(", ", uppercase),
                 SubTitle = "Uppercase - Copy to clipboard",
                 IcoPath = "Images\\icon.png",  //relative path to your plugin directory
                 Action = e => {
                     // after user selects the item
-                    Clipboard.SetText(id.ToUpper());
+                    Clipboard.SetText(String.Join("\n", uppercase) + "\n");
 
                     // return false to tell Wox not to hide thequery window, otherwise Wox will hide it automatically
                     return true;
                 }
             });
 
+            var braces = ids.Select(t => $"{{{t}}}");
             //Braces
             results.Add(new Result() {
-                Title = $"{{{id}}}",
+                Title = String.Join(", ", braces),
                 SubTitle = "With braces - Copy to clipboard",
                 IcoPath = "Images\\icon.png",  //relative path to your plugin directory
                 Action = e => {
                     // after user selects the item
-                    Clipboard.SetText($"{{{id}}}");
+                    Clipboard.SetText(String.Join("\n", braces) + "\n");
 
                     // return false to tell Wox not to hide thequery window, otherwise Wox will hide it automatically
                     return true;
@@ -58,13 +74,14 @@ namespace wox_uuid_generator
             });
 
             //Braces, uppercase
+            var uppercaseBraces = ids.Select(t => $"{{{t.ToUpper()}}}");
             results.Add(new Result() {
-                Title = $"{{{id.ToUpper()}}}",
+                Title = String.Join(", ", uppercaseBraces),
                 SubTitle = "With braces, uppercase - Copy to clipboard",
                 IcoPath = "Images\\icon.png",  //relative path to your plugin directory
                 Action = e => {
                     // after user selects the item
-                    Clipboard.SetText($"{{{id.ToUpper()}}}");
+                    Clipboard.SetText(String.Join("\n", uppercaseBraces) + "\n");
 
                     // return false to tell Wox not to hide thequery window, otherwise Wox will hide it automatically
                     return true;
@@ -72,13 +89,14 @@ namespace wox_uuid_generator
             });
 
             //no-hyphens
+            var noHyphens = ids.Select(t => t.Replace("-", ""));
             results.Add(new Result() {
-                Title = id.Replace("-", ""),
+                Title = String.Join(", ", noHyphens),
                 SubTitle = "No hyphens - Copy to clipboard",
                 IcoPath = "Images\\icon.png",  //relative path to your plugin directory
                 Action = e => {
                     // after user selects the item
-                    Clipboard.SetText(id.Replace("-", ""));
+                    Clipboard.SetText(String.Join("\n", noHyphens) + "\n");
 
                     // return false to tell Wox not to hide thequery window, otherwise Wox will hide it automatically
                     return true;
@@ -86,13 +104,14 @@ namespace wox_uuid_generator
             });
 
             //no-hyphens uppercase
+            var uppercaseNoHyphens = ids.Select(t => t.Replace("-", "").ToUpper());
             results.Add(new Result() {
-                Title = id.Replace("-", "").ToUpper(),
+                Title = String.Join(", ", uppercaseNoHyphens),
                 SubTitle = "No hyphens, uppercase - Copy to clipboard",
                 IcoPath = "Images\\icon.png",  //relative path to your plugin directory
                 Action = e => {
                     // after user selects the item
-                    Clipboard.SetText(id.Replace("-", "").ToUpper());
+                    Clipboard.SetText(String.Join("\n", uppercaseNoHyphens) + "\n");
 
                     // return false to tell Wox not to hide thequery window, otherwise Wox will hide it automatically
                     return true;
